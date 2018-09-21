f9ecf26c2f5d7a246fe01cc555ed66daea81e50b
PryFork <43276364+PryFork@users.noreply.github.com>
2018-09-21 11:59:40 -0400

    Create presenceUpdate.js

---
+++ backend/events/presenceUpdate.js
@@ -0,0 +1,12 @@
+const oldDetails = null;
+const newDetails = args.join();
+const timestamp = new Date();
+
+function findDetail() {
+if (!oldDetails) return throw new Error("Finding Old User Data So We Can Update User Details...")
+}
+
+function updatedDetail() {
+newDetails.post()
+console.log("Updated User Details At " + timestamp)
+}
